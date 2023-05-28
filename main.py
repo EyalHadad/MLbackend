041ebbeaeab7ebe6fd03dcb63a5f4612b6a83fe6
Ken Yaggel <chenyag40@gmail.com>
2023-05-29 00:01:02 +0300

    first commit into MLbackend: added training_model.py : for model training (prepare_training_data > train_model > evaluate_model) added first_model.model the model trained on: non_overlapping_sites_darnell_human_ViennaDuplex_negative_features.csv darnell_human.csv with default parameters

--- main.py
+++ main.py
@@ -3,12 +3,29 @@ from full_pipline_positive import full_pipline as full_pipline_positive
 from full_pipline_negative import full_pipline as full_pipline_negative
 
 
-def extract_features_from_sequences(input_details):
-    if input_details[5] != None:
-        res = full_pipline_positive(input_details)
+def extract_features_from_sequences(miRNA=None, mRNA=None, site=None):
+    # if input_details[5] != None:
+    #     res = full_pipline_positive(input_details)
+    # else:
+    #     res = full_pipline_negative(input_details,"to_delete")
+    # return res
+
+    columns = ['key', 'paper name', 'organism', 'miRNA ID', 'miRNA sequence', 'site', 'region', 'valid_row',
+               'full_mrna', 'Gene_ID', 'region count']
+    values = [None] * len(columns)
+    values[4] = miRNA
+    values[5] = site
+    values[8] = mRNA
+    dataset = pd.DataFrame([values], columns=columns)
+
+    if site != None:
+        result_df = full_pipline_positive(dataset)
     else:
-        res = full_pipline_negative(input_details,"to_delete")
-    return res
+        result_df = full_pipline_negative(dataset)
+        result_df.drop('site_x', axis=1, inplace=True)
+    pass
+
+    return result_df
 
 
 def get_prediction(seq_features,org_name):
@@ -32,4 +49,5 @@ def predict_sequences(miRNA_seq=None, mRNA_seq=None, site_seq=None,org_name="Hum
 
 
 if __name__ == '__main__':
-    predict_sequences('UGAGGUAGUAGGUUGUAUAGUU', 'ACCAACUCCUCUUGACCGAUGUAGAUCACCUGGAAUGCUUGAA', 'UAUUGCACUUGUCCCGGCCUGU')
+    print(predict_sequences('UGAGGUAGUAGGUUGUAUAGUU', 'ACCAACUCCUCUUGACCGAUGUAGAUCACCUGGAAUGCUUGAA', 'UAUUGCACUUGUCCCGGCCUGU'))
+
